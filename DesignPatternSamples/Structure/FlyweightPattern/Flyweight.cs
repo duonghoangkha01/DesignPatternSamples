b5816c8966f40e57804762631c30665ce58ca083
duonghoangkha01 <duonghoangkha2001@gmail.com>
2024-07-21 21:09:10 +0700

    Add Flyweight pattern

---
+++ DesignPatternSamples/Structure/FlyweightPattern/Flyweight.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace DesignPatternSamples.Structure.FlyweightPattern
+{
+    // The Flyweight stores a common portion of the state (also called intrinsic
+    // state) that belongs to multiple real business entities. The Flyweight
+    // accepts the rest of the state (extrinsic state, unique for each entity)
+    // via its method parameters.
+    public class Flyweight
+    {
+        private readonly Car _sharedState;
+
+        public Flyweight(Car car)
+        {
+            _sharedState = car;
+        }
+
+        public void Operation(Car uniqueState)
+        {
+            string s = JsonSerializer.Serialize(_sharedState);
+            string u = JsonSerializer.Serialize(uniqueState);
+            Console.WriteLine($"Flyweight: Displaying shared {s} and unique {u} state.");
+        }
+    }
+}
